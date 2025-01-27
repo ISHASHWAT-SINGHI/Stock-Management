c78f4fa60c816ff6bfd7fdf15d24536dec4f6e74
ISHASHWAT-SINGHI <145699719+ISHASHWAT-SINGHI@users.noreply.github.com>
2025-01-27 23:51:10 +0530

    view product and transaction functions

--- Codebase/stock-management.py
+++ Codebase/stock-management.py
@@ -2,17 +2,14 @@ import sqlite3
 import os
 import time
 
-# Current working directory 
-curr_directory=os.path.dirname(__file__)
-rel_path="Database/management.db"
-database_path=os.path.join(curr_directory,rel_path)
-conn=sqlite3.connect(database_path)
+# Create a database if not exist 
+conn=sqlite3.connect('management.db')
 cursor=conn.cursor()
 # Create table if not exist
 cursor.execute('''
                CREATE TABLE IF NOT EXISTS Products(
                PID integer Primary Key,
-               Company varchar(20)
+               Company varchar(20),
                Brand varchar(20),
                Product_name varchar(20),
                Unit_Price float,
@@ -20,4 +17,37 @@ cursor.execute('''
                Created_At timestamp
                )
                ''')
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS TransactionIn(
+               TransactionID integer Primary Key,
+               Company varchar(20),
+               CompanyGSTIN int,
+               Contact int,
+               PID integer,
+               TransactionTime timestamp
+               )
+               ''')
+
+# View Products
+def view_products():
+
+    # Retrieve and display all the products from the products table
+    cursor.execute("SELECT PID, Company, Brand, Product_name, Unit_Price, Quantity, (Unit_Price*Quantity) AS Total_Price FROM Products")
+    products=cursor.fetchall()
+    print(products)
+
+# View Transactions
+def view_transactions():
+
+    # Retrieve and display all the transactions from the transactionin table
+    cursor.execute("SELECT TransactionID, Company, CompanyGSTIN, Contact, PID, TransactionTime FROM TransactionIn")
+    transaction=cursor.fetchall()
+    print(transaction)
+
+# def add_product():
+    # Add products through transactions 
+
+# Main 
+view_products()
+view_transactions()
 conn.close()
