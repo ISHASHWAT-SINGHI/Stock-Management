8cc15bf4e7d8ea9f39d1d7f89b9b98ace4c1ee0e
ISHASHWAT-SINGHI <145699719+ISHASHWAT-SINGHI@users.noreply.github.com>
2025-01-27 17:50:09 +0530

    product table create command

--- Codebase/stock-management.py
+++ Codebase/stock-management.py
@@ -1,4 +1,23 @@
 import sqlite3
-conn=sqlite3.connect("management.db")
+import os
+import time
+
+# Current working directory 
+curr_directory=os.path.dirname(__file__)
+rel_path="Database/management.db"
+database_path=os.path.join(curr_directory,rel_path)
+conn=sqlite3.connect(database_path)
 cursor=conn.cursor()
+# Create table if not exist
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS Products(
+               PID integer Primary Key,
+               Company varchar(20)
+               Brand varchar(20),
+               Product_name varchar(20),
+               Unit_Price float,
+               Quantity integer,
+               Created_At timestamp
+               )
+               ''')
 conn.close()
